ec4d75cb521087ac8949ec7aaf8bba7101ae260e
Pahari47 <nachiketapahari00@gmail.com>
2024-03-19 19:44:13 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://www.hindustantimes.com/ht-img/img/2024/03/15/1600x900/INDIA-MARKETS-0_1651226937928_1710486937097.JPG"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}...
+              <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}} >
+                {source}
+              </span>
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
