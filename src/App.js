38d9f4a51d86015179d732e87ec9d7429b9b8d59
Pahari47 <nachiketapahari00@gmail.com>
2024-03-20 21:50:45 +0530

    class to func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
@@ -11,14 +11,11 @@ import {
   Route,
 } from "react-router-dom";
 
-export default class App extends Component {
-  state = {
-    progress:0
-  }
-  setProgress = (progress)=> {
-    this.setState({progress: progress})
-  }
-  render() {
+const App =()=> {
+ 
+  const [progress, setProgress] = useState(0)
+ 
+  
     return (
       <div>
         <Router>
@@ -26,20 +23,21 @@ export default class App extends Component {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
       />
         <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress}  key="general" pageSize={6} country="in" category="general"/></Route>
-          <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={6} country="in" category="business"/></Route>
-          <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={6} country="in" category="entertainment"/></Route>
-          <Route exact path="/general"><News setProgress={this.setProgress} key="general" pageSize={6} country="in" category="general"/></Route>
-          <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={6} country="in" category="health"/></Route>
-          <Route exact path="/science"><News setProgress={this.setProgress} key="science" pageSize={6} country="in" category="science"/></Route>
-          <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" pageSize={6} country="in" category="sports"/></Route>
-          <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" pageSize={6} country="in" category="technology"/></Route>
+          <Route exact path="/"><News setProgress={setProgress}  key="general" pageSize={6} country="in" category="general"/></Route>
+          <Route exact path="/business"><News setProgress={setProgress} key="business" pageSize={6} country="in" category="business"/></Route>
+          <Route exact path="/entertainment"><News setProgress={setProgress} key="entertainment" pageSize={6} country="in" category="entertainment"/></Route>
+          <Route exact path="/general"><News setProgress={setProgress} key="general" pageSize={6} country="in" category="general"/></Route>
+          <Route exact path="/health"><News setProgress={setProgress} key="health" pageSize={6} country="in" category="health"/></Route>
+          <Route exact path="/science"><News setProgress={setProgress} key="science" pageSize={6} country="in" category="science"/></Route>
+          <Route exact path="/sports"><News setProgress={setProgress} key="sports" pageSize={6} country="in" category="sports"/></Route>
+          <Route exact path="/technology"><News setProgress={setProgress} key="technology" pageSize={6} country="in" category="technology"/></Route>
         </Switch>
         </Router>
       </div>
     )
-  }
+  
 }
+export default App
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,114 +1,112 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {  } from "react";
+import { Link } from "react-router-dom";
 
-export class NavBar extends Component {
-  handleNavLinkClick = () => {
+const NavBar = () => {
+  let handleNavLinkClick = () => {
     setTimeout(() => {
       window.location.reload();
     }, 10);
   };
 
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              Stomnews
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    aria-current="page"
-                    to="/"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/business"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Business
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/entertainment"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Entertainment
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/general"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    General
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/health"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Health
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/science"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Science
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/sports"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Sports
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/technology"
-                    onClick={this.handleNavLinkClick}
-                  >
-                    Technology
-                  </Link>
-                </li>
-              </ul>
-            </div>
+  return (
+    <div>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Stomnews
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  to="/"
+                  onClick={handleNavLinkClick}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/business"
+                  onClick={handleNavLinkClick}
+                >
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/entertainment"
+                  onClick={handleNavLinkClick}
+                >
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/general"
+                  onClick={handleNavLinkClick}
+                >
+                  General
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/health"
+                  onClick={handleNavLinkClick}
+                >
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/science"
+                  onClick={handleNavLinkClick}
+                >
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/sports"
+                  onClick={handleNavLinkClick}
+                >
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/technology"
+                  onClick={handleNavLinkClick}
+                >
+                  Technology
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
